df40ffd9ea046e00eb81ff5e3ff3cf6af581c56a
Henry <hvelas93@gmail.com>
2021-02-26 17:02:42 -0500

    finished pup lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
 # Add your code here
+
+require 'Pry'
+
+class Dog
+
+    attr_accessor :name
+
+    @@all=[]
+
+    def initialize(name)
+        @name=name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        # puts @@all.map { |dog| dog.name }
+        @@all.each do |x|
+            #binding.pry
+            puts x.name
+        end
+        
+    end
+    
+    def save
+        @@all << self
+    end
+
+    
+end
